83807565694d819cf843f6e5078c7e4c13d80dc6
johnche <johnche@tencent.com>
2024-05-27 12:48:39 +0800

    verbose

--- windows_64.cmd
+++ windows_64.cmd
@@ -1,5 +1,7 @@
 git clone https://github.com/WebKit/webkit.git
 git checkout 8715d40f1d7038e9f43619d153210883228d17a6
 cd webkit
+echo =====[ apply patch ]=====
 node %WORKSPACE%\node-script\do-gitpatch.js -p %WORKSPACE%\patchs\webkit_vc_compile_for_8715d40f1d7038e9f43619d153210883228d17a6.patch
-perl Tools\Scripts\build-jsc
+echo =====[ apply patch ]=====
+perl Tools\Scripts\build-webkit --jsc-only --verbose
