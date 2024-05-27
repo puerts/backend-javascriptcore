015076885b2932159270178afedb5029206e4189
johnche <johnche@tencent.com>
2024-05-27 12:49:04 +0800

    logs

--- windows_64.cmd
+++ windows_64.cmd
@@ -3,5 +3,5 @@ git checkout 8715d40f1d7038e9f43619d153210883228d17a6
 cd webkit
 echo =====[ apply patch ]=====
 node %WORKSPACE%\node-script\do-gitpatch.js -p %WORKSPACE%\patchs\webkit_vc_compile_for_8715d40f1d7038e9f43619d153210883228d17a6.patch
-echo =====[ apply patch ]=====
+echo =====[ build jsc ]=====
 perl Tools\Scripts\build-webkit --jsc-only --verbose
