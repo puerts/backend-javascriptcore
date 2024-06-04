c0c213e65cac06cb576eb687fbc1e4d789123c7b
johnche <johnche@tencent.com>
2024-06-04 16:09:56 +0800

    workflow file error

--- windows_64.cmd
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/WebKit/webkit.git
-git checkout 8715d40f1d7038e9f43619d153210883228d17a6
-cd webkit
-echo =====[ apply patch ]=====
-node %WORKSPACE%\node-script\do-gitpatch.js -p %WORKSPACE%\patchs\webkit_vc_compile_for_8715d40f1d7038e9f43619d153210883228d17a6.patch
-echo =====[ build jsc ]=====
-perl Tools\Scripts\build-webkit --jsc-only --verbose
