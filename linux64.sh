f1c2610f5c6a76450f37e4cb9ffbf021982fa120
johnche <johnche@tencent.com>
2024-06-05 10:51:48 +0800

    fix upload fail, create asset fail

--- linux64.sh
+++ linux64.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[ -z "$WORKSPACE" ] && WORKSPACE="$( cd "$( dirname "$0" )"/.. && pwd )"
 
 npm i
 if [ ! -d "webkit" ]; then
@@ -6,21 +7,19 @@ if [ ! -d "webkit" ]; then
     git clone https://github.com/WebKit/webkit.git
     cd webkit
     git reset --hard 8715d40f1d70
-    echo =====[ apply patch ]=====
-    node $(dirname $0)/node-script/do-gitpatch.js -p $(dirname $0)/patchs/webkit_vc_compile_for_8715d40f1d7038e9f43619d153210883228d17a6.patch
     cd ..
 fi
 echo =====[ build jsc ]=====
 cd webkit
 perl Tools/Scripts/build-webkit --jsc-only --cmakeargs="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++" --verbose
-mkdir -p $(dirname $0)/jsc_bin/jsc/include
-mkdir -p $(dirname $0)/jsc_bin/jsc/lib/linux64
-cp -r Source/bmalloc/bmalloc $(dirname $0)/jsc_bin/jsc/include/
-cp -r Source/WTF/wtf $(dirname $0)/jsc_bin/jsc/include/
-cp -r Source/WTF/icu/unicode $(dirname $0)/jsc_bin/jsc/include/
-cp -r WebKitBuild/JSCOnly/Release/JavaScriptCore/Headers/JavaScriptCore $(dirname $0)/jsc_bin/jsc/include/
-cp -r WebKitBuild/JSCOnly/Release/JavaScriptCore/PrivateHeaders/JavaScriptCore/* $(dirname $0)/jsc_bin/jsc/include/JavaScriptCore/
-cp $(dirname $0)/misc/cmakeconfig.h $(dirname $0)/jsc_bin/jsc/include/JavaScriptCore/cmakeconfig.h
-cp WebKitBuild/JSCOnly/Release/cmakeconfig.h $(dirname $0)/jsc_bin/jsc/include/JavaScriptCore/cmakeconfig_linux64.h
-cp WebKitBuild/JSCOnly/Release/lib/libJavaScriptCore.so* $(dirname $0)/jsc_bin/jsc/lib/linux64
+mkdir -p $WORKSPACE/jsc_bin/jsc/include
+mkdir -p $WORKSPACE/jsc_bin/jsc/lib/linux64
+cp -r Source/bmalloc/bmalloc $WORKSPACE/jsc_bin/jsc/include/
+cp -r Source/WTF/wtf $WORKSPACE/jsc_bin/jsc/include/
+cp -r Source/WTF/icu/unicode $WORKSPACE/jsc_bin/jsc/include/
+cp -r WebKitBuild/JSCOnly/Release/JavaScriptCore/Headers/JavaScriptCore $WORKSPACE/jsc_bin/jsc/include/
+cp -r WebKitBuild/JSCOnly/Release/JavaScriptCore/PrivateHeaders/JavaScriptCore/* $WORKSPACE/jsc_bin/jsc/include/JavaScriptCore/
+cp $WORKSPACE/misc/cmakeconfig.h $WORKSPACE/jsc_bin/jsc/include/JavaScriptCore/cmakeconfig.h
+cp WebKitBuild/JSCOnly/Release/cmakeconfig.h $WORKSPACE/jsc_bin/jsc/include/JavaScriptCore/cmakeconfig_linux64.h
+cp WebKitBuild/JSCOnly/Release/lib/libJavaScriptCore.so* $WORKSPACE/jsc_bin/jsc/lib/linux64
 cd ..
