506e3ff1e5ab2979422cac782982a29ddd63293a
johnche <johnche@tencent.com>
2024-06-06 17:23:10 +0800

    build type release

--- test/make_linux64.sh
+++ test/make_linux64.sh
@@ -1,4 +1,4 @@
 mkdir -p build_linux64 && cd build_linux64
-cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=Debug ../
+cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ../
 cd ..
-cmake --build build_linux64 --config Debug
+cmake --build build_linux64 --config Release
